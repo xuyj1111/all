faa582d8129de541ded181eea15a3dda13de7d87
yujun.xu <yujun.xu@linkflowtech.com>
2021-12-21 16:44:25 +0800

    Basic environment

---
+++ src/test/java/xu/all/AllApplicationTests.java
@@ -0,0 +1,13 @@
+package xu.all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

81d8f94bb65d9458153112f65bfa88fa27eaf0f8
yujun.xu <yujun.xu@linkflowtech.com>
2021-12-21 18:28:45 +0800

    move a little

---
+++ src/main/java/xu/all/HelloWorldController.java
@@ -0,0 +1,15 @@
+package xu.all;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello_world")
+public class HelloWorldController {
+
+    @PostMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}

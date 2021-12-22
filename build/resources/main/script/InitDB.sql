8f21f4d6a41264e15dda1d7f57ca07349ce9fec6
yujun.xu <yujun.xu@linkflowtech.com>
2021-12-22 16:20:46 +0800

    clear

---
+++ build/resources/main/script/InitDB.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS xu CHARACTER SET utf8mb4 COLLATE utf8mb4_german2_ci;
+USE xu;
+
+DROP TABLE IF EXISTS test;
+CREATE TABLE test
+(
+    `id`           bigint(20) NOT NULL AUTO_INCREMENT,
+    `name`         varchar(50) DEFAULT NULL,
+    `date_created` datetime   NOT NULL,
+    `last_updated` datetime   NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO test(name, date_created, last_updated)
+VALUES ('张三', now(), now());

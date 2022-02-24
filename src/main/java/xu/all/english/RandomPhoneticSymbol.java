4f35a2a3805ca12ff4854d509d8eb85542bac927
yujun.xu <yujun.xu@linkflowtech.com>
2022-02-24 23:22:34 +0800

    学英语音标

---
+++ src/main/java/xu/all/english/RandomPhoneticSymbol.java
@@ -0,0 +1,7 @@
+package xu.all.english;
+
+public class RandomPhoneticSymbol {
+    private static String[] symbols = new String[]{"/i:/", "/ɪ/",
+            "/e/", "/æ/", "/ɜ:/", "/ə/", "/ʌ/", "/ɔ:/", "/ɒ/", "/u:/",
+            "/ʊ/", "/ɑ:/", "/aɪ/", "/eɪ/", "/aʊ/", "/əʊ/", "/ɔɪ/", "/ɪə/", "/eə/", "/ʊə/"};
+}

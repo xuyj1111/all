bb81984cf27228869f7e2eb792c622ae8504e833
yujun.xu <yujun.xu@linkflowtech.com>
2022-02-25 11:50:03 +0800

    随机音标

--- src/main/java/xu/all/english/RandomPhoneticSymbol.java
+++ src/main/java/xu/all/english/RandomPhoneticSymbol.java
@@ -1,7 +1,24 @@
 package xu.all.english;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class RandomPhoneticSymbol {
-    private static String[] symbols = new String[]{"/i:/", "/ɪ/",
-            "/e/", "/æ/", "/ɜ:/", "/ə/", "/ʌ/", "/ɔ:/", "/ɒ/", "/u:/",
-            "/ʊ/", "/ɑ:/", "/aɪ/", "/eɪ/", "/aʊ/", "/əʊ/", "/ɔɪ/", "/ɪə/", "/eə/", "/ʊə/"};
+    private static String[] symbols = new String[]{
+            "/ɑ:/", "/ɔ:/", "/ɜ:/", "/i:/", "/u:/",
+            "/ʌ/", "/ɒ/", "/ə/", "/ɪ/", "/ʊ/", "/e/", "/æ/",
+            "/eɪ/", "/aɪ/", "/ɔɪ/", "/ɪə/", "/eə/", "/ʊə/", "/əʊ/", "/aʊ/",
+            "/p/", "/t/", "/k/", "/f/", "/θ/", "/s/", "/∫/", "/ts/", "/t∫/", "/tr/",
+            "/b/", "/d/", "/g/", "/v/", "/ð/", "/z/", "/ʒ/", "/dz/", "/dʒ/", "/dr/",
+            "/m/", "/n/", "/ŋ/",
+            "/h/", "/l/", "/r/",
+            "/j/", "/w/"};
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            int index = new Random().nextInt(symbols.length);
+            System.out.println(symbols[index]);
+            System.in.read();
+        }
+    }
 }

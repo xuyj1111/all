5fdd2a4ab3fc698db87686f27fea43f77ae9f6f9
yujun.xu <yujun.xu@linkflowtech.com>
2022-03-30 15:47:41 +0800

    开启feign，增加forward demo

--- src/main/java/xu/all/AllApplication.java
+++ src/main/java/xu/all/AllApplication.java
@@ -2,8 +2,10 @@ package xu.all;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AllApplication {
 
     public static void main(String[] args) {

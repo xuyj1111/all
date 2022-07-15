8d28bd6fd557f124e44ec3c02ac381b92195ed3d
yujun.xu <yujun.xu@linkflowtech.com>
2022-07-15 10:25:52 +0800

    【暂存activiti】好麻烦，以后再学

---
+++ src/main/java/xu/all/config/ActivitiConfig.java
@@ -0,0 +1,71 @@
+package xu.all.config;
+
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.spring.ProcessEngineFactoryBean;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+public class ActivitiConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public SpringProcessEngineConfiguration springProcessEngineConfiguration() {
+        SpringProcessEngineConfiguration spec = new SpringProcessEngineConfiguration();
+        spec.setDataSource(dataSource);
+        spec.setTransactionManager(platformTransactionManager);
+        spec.setDatabaseSchema("true");
+        Resource[] resources = null;
+        // 启动自动部署流程
+        try {
+            resources = new PathMatchingResourcePatternResolver().getResources("classpath*:bpmn/*.bpmn");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        spec.setDeploymentResources(resources);
+        return spec;
+    }
+
+    @Bean
+    public ProcessEngineFactoryBean processEngine() {
+        ProcessEngineFactoryBean processEngineFactoryBean = new ProcessEngineFactoryBean();
+        processEngineFactoryBean.setProcessEngineConfiguration(springProcessEngineConfiguration());
+        return processEngineFactoryBean;
+    }
+
+    @Bean
+    public RepositoryService repositoryService() throws Exception {
+        return processEngine().getObject().getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService() throws Exception {
+        return processEngine().getObject().getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService() throws Exception {
+        return processEngine().getObject().getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService() throws Exception {
+        return processEngine().getObject().getHistoryService();
+    }
+}

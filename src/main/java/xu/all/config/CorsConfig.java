73ac541891e1ec9478fb1d0a0e00b7d7c4f0dc49
yujun.xu <yujun.xu@linkflowtech.com>
2023-02-24 18:05:59 +0800

    增加拦截器，实现请求跨域

---
+++ src/main/java/xu/all/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package xu.all.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS").allowCredentials(true).maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
